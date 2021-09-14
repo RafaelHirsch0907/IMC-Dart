5304427e0611a57db5c3731044fede5c5ce907d4
Rafael Moraes Hirsch <rafael.hirsch@ucsal.edu.br>
2021-09-14 10:28:59 -0300

    Código dividido em funções.

--- imc.dart
+++ imc.dart
@@ -1,30 +1,43 @@
 import 'dart:io';
 
 main(){
+ calculoDeImc();
+}
+
+calculoDeImc(){
+
+  print("Digite seu peso e sua altura: ");
+  String? textPeso = stdin.readLineSync(), textAltura = stdin.readLineSync();
+  if(textPeso != null && textAltura != null){
+    int peso = int.parse(textPeso);
+    double altura = double.parse(textAltura);
+    double imc = calcImc(peso, altura);
+    imprimirResultado(imc);
+  }
+}
+
+double calcImc(int peso, double altura){
   /*Pegar PESO
     Pegar ALTURA
     Realizar CÁLCULO
     Retornar RESULTADO
   */
-  print("Digite seu peso e sua altura: ");
-  String? textPeso = stdin.readLineSync(), textAltura = stdin.readLineSync();
-  if(textPeso != null && textAltura != null){
-    var peso = int.parse(textPeso);
-    var altura = double.parse(textAltura);
-    var calcImc = peso / (altura * altura);
-   if(calcImc < 18.5){
-     print(calcImc);
-     print("Abaixo do peso");
-   }else if(calcImc > 18.5 && calcImc < 24.9){
-     print("Peso normal");
-   }else if(calcImc > 25 && calcImc < 29.9){
-     print("Sobrepeso");
-   }else if(calcImc > 30 && calcImc < 34.9){
-     print("Obesidade 1");
-   }else if(calcImc > 35 && calcImc < 39.9){
-     print("Obesidade 2");
-   }else{
-     print("Obesidade 3");
-   }
+  return peso / (altura * altura);
+}
+
+imprimirResultado(double imc){
+  if(imc < 18.5){
+    print(imc);
+    print("Abaixo do peso");
+  }else if(imc > 18.5 && imc < 24.9){
+    print("Peso normal");
+  }else if(imc > 25 && imc < 29.9){
+    print("Sobrepeso");
+  }else if(imc > 30 && imc < 34.9){
+    print("Obesidade 1");
+  }else if(imc > 35 && imc < 39.9){
+    print("Obesidade 2");
+  }else{
+    print("Obesidade 3");
   }
 }
